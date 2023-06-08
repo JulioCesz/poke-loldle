3ef7aaeeeffc201ff789457c0c3fadc9cc344a77
Charles Reis Ribeiro <charles.r.ribeiro@gmail.com>
2023-06-07 23:47:55 -0300

    adicionado README.md, lintados arquivos do projeto

--- script.js
+++ script.js
@@ -1,51 +1,52 @@
 // Lista de Pokémon e suas tipagens
 const pokemonList = [
-    { name: 'Pikachu', type: 'Elétrico' },
-    { name: 'Bulbasaur', type: 'Planta' },
-    { name: 'Charizard', type: 'Fogo' },
-    { name: 'Squirtle', type: 'Água' },
-    // Adicione mais Pokémon à lista, se desejar
-  ];
-  
-  // Função para escolher um Pokémon aleatório da lista
-  function chooseRandomPokemon() {
-    const randomIndex = Math.floor(Math.random() * pokemonList.length);
-    return pokemonList[randomIndex];
+  { name: 'Pikachu', type: 'Elétrico' },
+  { name: 'Bulbasaur', type: 'Planta' },
+  { name: 'Charizard', type: 'Fogo' },
+  { name: 'Squirtle', type: 'Água' },
+  // Adicione mais Pokémon à lista, se desejar
+];
+
+// Função para escolher um Pokémon aleatório da lista
+function chooseRandomPokemon() {
+  const randomIndex = Math.floor(Math.random() * pokemonList.length);
+  return pokemonList[randomIndex];
+}
+
+// Função para comparar a tipagem dos Pokémon
+function compareTypes(guess, target) {
+  if (guess.type === target.type) {
+    return 'Você acertou!';
+  } if (guess.type === 'Elétrico' && target.type === 'Planta') {
+    return 'O Pokémon que você escolheu é de tipagem mais fraca.';
+  } if (guess.type === 'Fogo' && target.type === 'Elétrico') {
+    return 'O Pokémon que você escolheu é de tipagem mais fraca.';
+  } if (guess.type === 'Água' && target.type === 'Fogo') {
+    return 'O Pokémon que você escolheu é de tipagem mais fraca.';
   }
-  
-  // Função para comparar a tipagem dos Pokémon
-  function compareTypes(guess, target) {
-    if (guess.type === target.type) {
-      return "Você acertou!";
-    } else if (guess.type === "Elétrico" && target.type === "Planta") {
-      return "O Pokémon que você escolheu é de tipagem mais fraca.";
-    } else if (guess.type === "Fogo" && target.type === "Elétrico") {
-      return "O Pokémon que você escolheu é de tipagem mais fraca.";
-    } else if (guess.type === "Água" && target.type === "Fogo") {
-      return "O Pokémon que você escolheu é de tipagem mais fraca.";
-    } else {
-      return "O Pokémon que você escolheu é de tipagem mais forte.";
-    }
+  return 'O Pokémon que você escolheu é de tipagem mais forte.';
+}
+
+// Função principal do jogo
+function playGuessThePokemon() {
+  const targetPokemon = chooseRandomPokemon();
+  const guess = prompt('Digite o nome de um Pokémon:');
+
+  if (!guess) {
+    return; // O jogador cancelou o jogo
   }
-  
-  // Função principal do jogo
-  function playGuessThePokemon() {
-    const targetPokemon = chooseRandomPokemon();
-    const guess = prompt('Digite o nome de um Pokémon:');
-    
-    if (!guess) {
-      return; // O jogador cancelou o jogo
-    }
-    
-    const guessedPokemon = pokemonList.find(pokemon => pokemon.name.toLowerCase() === guess.toLowerCase());
-    
-    if (!guessedPokemon) {
-      console.log("Esse Pokémon não existe.");
-      return;
-    }
-    
-    console.log(compareTypes(guessedPokemon, targetPokemon));
+
+  const guessedPokemon = pokemonList.find(
+    (pokemon) => pokemon.name.toLowerCase() === guess.toLowerCase()
+  );
+
+  if (!guessedPokemon) {
+    console.log('Esse Pokémon não existe.');
+    return;
   }
-  
-  // Execução do jogo
-  playGuessThePokemon;
+
+  console.log(compareTypes(guessedPokemon, targetPokemon));
+}
+
+// Execução do jogo
+playGuessThePokemon();
